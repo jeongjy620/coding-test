3f9153b6db311caae29fe5d688aced305ec4b5eb
정준영 <jeongjy@mrblue.com>
2025-10-15 08:42:19 +0900

    feat: 땅따먹기

--- src/test/java/junyoung/week28/Week28JeongjySolutionTest.java
+++ src/test/java/junyoung/week28/Week28JeongjySolutionTest.java
@@ -3,7 +3,11 @@ package junyoung.week28;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +31,29 @@ class Week28JeongjySolutionTest {
 
         // then
         assertThat(result).isEqualTo(expected);
+    }
+
+    @DisplayName("땅따먹기")
+    @ParameterizedTest
+    @MethodSource("HopscotchProvider")
+    void Hopscotch(int[][] land, int expected) {
+        // given & when
+        int result = solution.Hopscotch(land);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> HopscotchProvider() {
+        return Stream.of(
+                Arguments.of(new int[][]{
+                        {1, 2, 3, 5},
+                        {5, 6, 7, 8},
+                        {4, 3, 2, 1}
+                }, 16),
+                Arguments.of(new int[][]{
+                        {1, 2, 3, 5}
+                }, 5)
+        );
     }
 }

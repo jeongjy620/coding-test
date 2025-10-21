924cea95850c64afa7baa05b7fdc7006edc3b214
정준영 <jeongjy@mrblue.com>
2025-10-22 08:45:40 +0900

    feat: 두개 뽑아서 더하기

--- src/main/java/junyoung/week29/Week29JeongjySolution.java
+++ src/main/java/junyoung/week29/Week29JeongjySolution.java
@@ -1,6 +1,8 @@
 package junyoung.week29;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Week29JeongjySolution {
     public int budget(int[] d, int budget) {
@@ -15,5 +17,19 @@ public class Week29JeongjySolution {
             }
         }
         return count;
+    }
+
+    public int[] pickTwoAndSum(int[] numbers) {
+        Set<Integer> sumNumbers = new HashSet<>();
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i + 1; j < numbers.length; j++) {
+                sumNumbers.add(numbers[i] + numbers[j]);
+            }
+        }
+        return sumNumbers.stream()
+                .sorted()
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
--- src/test/java/junyoung/week29/Week29JeongjySolutionTest.java
+++ src/test/java/junyoung/week29/Week29JeongjySolutionTest.java
@@ -3,12 +3,12 @@ package junyoung.week29;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Week29JeongjySolutionTest {
 
@@ -32,5 +32,21 @@ class Week29JeongjySolutionTest {
         );
     }
 
+    @DisplayName("두 개 뽑아서 더하기")
+    @ParameterizedTest
+    @MethodSource("pickTwoAndSumProvider")
+    void pickTwoAndSum(int[] numbers, int[] expected) {
+        // given & when
+        int[] result = solution.pickTwoAndSum(numbers);
 
+        // then
+        assertArrayEquals(expected, result);
+    }
+
+    private static Stream<Arguments> pickTwoAndSumProvider() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 1, 3, 4, 1}, new int[]{2, 3, 4, 5, 6, 7}),
+                Arguments.of(new int[]{5, 0, 2, 7}, new int[]{2, 5, 7, 9, 12})
+        );
+    }
 }
